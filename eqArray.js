0ce7e78e52e1022158d582c8ed20fd7ab6727b8d
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-05-31 20:25:28 +0000

    eqArray.js added

---
+++ eqArray.js
@@ -0,0 +1,33 @@
+
+
+
+// const assertEqual = function(actual, expected) {
+//   if (actual !== expected) {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   } else {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   }
+// };
+
+
+const eqArrays = function(actual, expected) {
+
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let x = 0; x < expected.length; x++) {
+      if (actual[x] !== expected[x]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// checking for eqArray return value 
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should PASS
