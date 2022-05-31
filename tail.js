796396b8107a39d8d4a9cd9a90d17f43ae50109b
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-05-31 03:04:43 +0000

    added tail function

---
+++ tail.js
@@ -0,0 +1,35 @@
+
+
+// // FUNCTION IMPLEMENTATION
+// const assertEqual = function(actual, expected) {
+ 
+//   for (let x = 0; x < expected.length; x++) {
+//     if (actual[x] !== expected[x]) {
+//       console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//       break;
+//     }
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   }
+// };
+
+
+
+// tail function
+const tail = function(array) {
+  let newArray = [];
+  for (let x  = 1; x < array.length; x++) {
+    newArray.push(array[x]);
+  }
+  return newArray;
+};
+
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+
+// // array motification test
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// console.log(words)
+// assertEqual(words.length, 3); // original array should still have 3 elements!
