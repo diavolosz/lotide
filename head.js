dc2b47e5a29e29d627e59d139ef8193c0f12f534
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-05-31 02:37:00 +0000

    added head.js function

---
+++ head.js
@@ -0,0 +1,30 @@
+
+
+// // FUNCTION IMPLEMENTATION
+// const assertEqual = function(actual, expected) {
+//   if (actual !== expected) {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   } else {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   }
+// };
+
+
+
+
+// // FUNCTION IMPLEMENTATION
+// const head = function(array) {
+//   return array[0]
+// };
+
+// // TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
+
+// FUNCTION IMPLEMENTATION
+const head = function(array) {
+  return array[0]
+};
+
