fcdf884521cbbc72e65f9af27ad1033ca7db22b2
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-01 20:26:42 +0000

    added letterPosition.js

---
+++ letterPosition.js
@@ -0,0 +1,53 @@
+
+
+
+// assetEq Func
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } 
+
+  for (let x = 0; x < array2.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (!eqArrays(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+
+//-----------------------------------------------------------------------------
+
+//actual function
+const letterPositions = function(sentence) {
+  const object = {};
+
+  for (let x = 0; x < sentence.length; x++) {
+    if (sentence[x] === " ") {
+      continue;
+    }
+    if (object[sentence[x]]) {
+      object[sentence[x]].push(x);
+    } else {
+      object[sentence[x]] = [];
+      object[sentence[x]].push(x);
+    }
+  }
+  return object
+};
+
+let result = letterPositions("hello")
+assertArraysEqual(result.l, [2, 3])
+
+
