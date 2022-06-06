86ed67197887f3398cc96aaceea6ca27cd1a4fd6
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-06 18:47:34 +0000

    refactored eqArray test codes to incorporate mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,16 +1,43 @@
-let assertEqual = require("../assertEqual")
-let eqArrays = require("../eqArrays")
 
 
 // checking for eqArray return value 
-let testSet1 = eqArrays([[2, 3], [4]], [[2, 3], [4]]) // => true
-let testSet2 = eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]) // => false
-let testSet3 = eqArrays([[2, 3], [4]], [[2, 3], 4]) // => false
+// let testSet1 = eqArrays([[2, 3], [4]], [[2, 3], [4]]) // => true
+// let testSet2 = eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]) // => false
+// let testSet3 = eqArrays([[2, 3], [4]], [[2, 3], 4]) // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // => should fail
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false); // => should fail
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)
+
+const assert = require("chai").assert;
+const expect = require("chai").expect;
+const should = require("chai").should;
+
+let eqArrays = require("../eqArrays")
+
+describe ("testing eqArray function", () => {
+
+  it("should return true if array is equal", () => { 
+    let input1 = [[2, 3], [4]]
+    let input2 = [[2, 3], [4]]
+
+    let output = eqArrays(input1, input2);
+    assert.isTrue(output)
+  });
+
+
+  it("should return false if array is not equal", () => { 
+    let input1 = [[2, 3], [4]]
+    let input2 = [[2, 3], [4, 5]]
+    
+    let output = eqArrays(input1, input2);
+    assert.isFalse(output)
+  });
+
+  it("should return false if array is not equal", () => { 
+    let input1 = [[2, 3], [4]]
+    let input2 = [[2, 3], 4]
+    
+    let output = eqArrays(input1, input2);
+    assert.isFalse(output)
+  });
+
+});
