bde296541cd8624f8ec80b909aad16e637ce8c86
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-06 19:43:14 +0000

    sync with local files and removed test cases

--- countOnly.js
+++ countOnly.js
@@ -1,29 +1,3 @@
-
-
-// // assetEq Func
-// const assertEqual = function(actual, expected) {
-//   if (actual !== expected) {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   } else {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   }
-// };
-
-
-
-
-//-----------------------------------------------------------------------------
-
-
-
-
-
-
-
-// function starts here 
-
-let assertEqual = require("./assertEqual")
-
 const countOnly = function(allItems, itemsToCount) {
 
   let keepCount = {}
@@ -39,31 +13,7 @@ const countOnly = function(allItems, itemsToCount) {
       }
     }
   }
-  console.log(keepCount)
   return keepCount
 }
 
 module.exports = countOnly
-
-
-// // data
-// const firstNames = [
-//   "Karl",
-//   "Salima",
-//   "Agouhanna",
-//   "Fang",
-//   "Kavith",
-//   "Jason",
-//   "Salima",
-//   "Fang",
-//   "Joe"
-// ];
-
-
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-// // test case
-// assertEqual(result1["Jason"], 1);
-// assertEqual(result1["Karima"], undefined);
-// assertEqual(result1["Fang"], 2);
-// assertEqual(result1["Agouhanna"], undefined);
