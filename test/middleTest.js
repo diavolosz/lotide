83d7cbbd2234bfaa443e76dc67ace1ba18f38a2a
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-06 19:02:20 +0000

    refactored test codes to incorporate mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,33 @@
 
-let assertArraysEqual = require("../assertArraysEqual")
+const assert = require("chai").assert;
+const expect = require("chai").expect;
+const should = require("chai").should;
+
 let middle = require("../middle")
 
+describe ("Placeholder", () => {
 
-// test case
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5])
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-assertArraysEqual(middle(["apple", 2, "banana", "4"]), [2, "banana"])
+  it("should return 2 number when array is even", () => {
+    let input = [1, 2, 3, 4, 5, 6];
+    let output = middle(input);
+    let expectedOutput = [3, 4];
+    expect(output).to.deep.equal(expectedOutput);
+  });
 
+  it("should return 1 number when array is even", () => {
+    let input = [1, 2, 3, 4, 5];
+    let output = middle(input);
+    let expectedOutput = [3];
+    expect(output).to.deep.equal(expectedOutput);
+  });
+
+  it("should return middle items even when strings are incorporated", () => {
+    let input = ["apple", 2, "banana", "4"];
+    let output = middle(input);
+    let expectedOutput = [2, "banana"];
+    expect(output).to.deep.equal(expectedOutput);
+  });
+
+});
 
 
