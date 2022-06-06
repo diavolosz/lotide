ed5586ee0d1b749e71e0fcf3c903236038d18ad8
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-06 19:30:25 +0000

    fixed file name

--- countLetters.js
+++ countLetters.js
@@ -1,25 +1,3 @@
-
-
-
-
-
-// // assetEq Func
-// const assertEqual = function(actual, expected) {
-//   if (actual !== expected) {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   } else {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   }
-// };
-
-
-
-
-//-----------------------------------------------------------------------------
-
-let assertEqual = require("./assertEqual")
-
-
 let countLetter = function(stringInput) {
 
   let object = {};
@@ -36,11 +14,5 @@ let countLetter = function(stringInput) {
 };
 
 
-// console.log(countLetter("lighthouse in the house"));
-// const result = countLetter("lighthouse in the house");
-// assertEqual(result["i"], 2);
-// assertEqual(result["h"], 4);
-
-
-
+module.exports = countLetter
 
