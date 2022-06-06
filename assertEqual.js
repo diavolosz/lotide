10aa7c46d30755a103781c74abd36b26d9e6afce
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-06 19:42:09 +0000

    sync with local files and removed test cases

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,3 @@
-
-
-
-
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
@@ -11,11 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+
+
 module.exports = assertEqual;
-
-
-// // TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
 
