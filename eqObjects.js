1a3efedd87f3bfab265a0e0a570cc9a4e37f6a06
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-06 20:25:57 +0000

    sync with local files and removed test cases

--- eqObjects.js
+++ eqObjects.js
@@ -1,12 +1,5 @@
-
-
-
-//--------------------------------------------------------------------
-
-let assertEqual = require("./assertEqual")
 let eqArrays = require("./eqArrays")
 
-// function implementation 
 const eqObjects = function(object1, object2) {
 
     let keyArray1 = Object.keys(object1)
@@ -37,29 +30,3 @@ const eqObjects = function(object1, object2) {
 module.exports = eqObjects
 
 
-// //test case
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// eqObjects(ab, ba); // => true
-
-// const abc = { a: "1", b: "2", c: "3" };
-// eqObjects(ab, abc); // => false
-
-
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// eqObjects(cd, dc); // => true
-
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// eqObjects(cd, cd2); // => false
-
-
-// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true) // => true
-// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false) // => false
-// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
-// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 3 }), false) // => false
-
-// assertEqual(eqObjects(cd, dc), true)
-// assertEqual(eqObjects(cd, cd2), false)
-// assertEqual(eqObjects(ab, ba), true)
-// assertEqual(eqObjects(ab, abc), false)
